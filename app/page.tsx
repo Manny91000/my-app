8e09159fb291ad8113bbcdea47e1d3d67342018e
Manny91000 <77017764+Manny91000@users.noreply.github.com>
2025-07-17 11:12:42 -0400

    cambios

--- app/page.tsx
+++ app/page.tsx
@@ -26,4 +26,15 @@ const Home = () => {
 }
 
 
+import { z } from "zod"
+
+export const registerSchema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+
+
+
 export default Home
